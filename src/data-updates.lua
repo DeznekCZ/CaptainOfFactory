8a806170442558a69817092df086e8775091ca21
Zdeněk Novotný <zde.nov.chr@seznam.cz>
2024-11-27 23:01:30 +0100

    Version 0.0.3

--- src/data-updates.lua
+++ src/data-updates.lua
@@ -1,5 +1,11 @@
 
+local slag_item = { type="item", name="stone", amount=1 }
+local iron_temperature = 1500;
+local copper_temperature = 1100;
+
 data.raw.recipe["iron-plate"] = nil;
+data.raw.recipe["copper-plate"] = nil;
+
 data:extend{
     {
         type="recipe",
@@ -8,10 +14,26 @@ data:extend{
         category="smelting",
         energy_required=5,
         ingredients={
-            { type="item", name="iron-ore", amount=5 }
+            { type="item", name="iron-ore", amount=8 }
         },
         results={
-            { type="fluid", name="molten-iron", amount=50, temperature=2000 }
+            { type="fluid", name="molten-iron", amount=50, temperature=iron_temperature },
+            slag_item
+        },
+        enabled=true
+    },
+    {
+        type="recipe",
+        name="molten-copper-simple",
+        icon="__space-age__/graphics/icons/fluid/molten-copper.png",
+        category="smelting",
+        energy_required=5,
+        ingredients={
+            { type="item", name="copper-ore", amount=8 }
+        },
+        results={
+            { type="fluid", name="molten-copper", amount=50, temperature=copper_temperature },
+            slag_item
         },
         enabled=true
     }
@@ -28,32 +50,37 @@ furnace.fluid_boxes = {
                 flow_direction="output",
                 direction=defines.direction.east,
                 position={x=0.5,y=0.5},
+                connection_category="molten-channel"
             }
         },
     }
 }
 
-local function stone_form(planet, cooling_time, cooled, source, source_count, result, result_count)
+---comment
+---@param parameters { planet:string, cooling_time:integer, cooled:boolean, source:{ fluid:string, amount:integer, temperature:integer }, result:{ item:string, amount:integer }}
+---@return table
+local function stone_form(parameters)
     local icons = {
         { icon="__base__/graphics/icons/stone.png" },
-        { icon="__base__/graphics/icons/"..result..".png" },
+        { icon="__base__/graphics/icons/"..parameters.result.item..".png" },
     }
-    local form_name = planet .. "-" .. result .. "-cast-form";
-    local form_item_name = form_name .. "-item";
-    local form_recipe_name = form_name .. "-recipe";
-    local cast_recipe_name = planet .. "-" .. result .. "-cast-recipe";
-    local cast_category_name = planet .. "-cast-category";
+    local form_name = parameters.planet .. "-" .. parameters.result.item .. "-cast-form";
+    local cast_recipe_name = parameters.planet .. "-" .. parameters.result.item .. "-cast-recipe";
+    local cast_category_name = parameters.planet .. "-cast-category";
+
+    local cooling_name = {"entity-name.form", {"item-name." .. parameters.result.item}, {"fluid-name."..parameters.source.fluid}};
+    local cooling_description = {"entity-description.cooling", {"item-name." .. parameters.result.item}, {"fluid-name."..parameters.source.fluid}};
 
     local assembler = {
         name=form_name,
-        localised_name={"entity-name.form", {"item-name." .. result}, {"fluid-name."..source}},
-        localised_decription={"entity-description.cooling", {"item-name." .. result}, {"fluid-name."..source}},
+        localised_name=cooling_name,
+        localised_decription=cooling_description,
         type="assembling-machine",
         icons=icons,
         ingredient_count=1, --resource and coolant
         fluid_boxes = {
             {
-                volume=source_count,
+                volume=parameters.source.amount,
                 production_type="input",
                 pipe_connections={
                     {
@@ -61,6 +88,7 @@ local function stone_form(planet, cooling_time, cooled, source, source_count, re
                         flow_direction="input",
                         direction=defines.direction.west,
                         position={x=0,y=0},
+                        connection_category="molten-channel"
                     }
                 },
             }
@@ -69,11 +97,11 @@ local function stone_form(planet, cooling_time, cooled, source, source_count, re
         hidden = false,
         subgroup = "forms",
         placeable_by={
-            item=form_item_name,
+            item=form_name,
             count=1
         },
         minable={
-            result=form_item_name,
+            result=form_name,
             count=1,
             mining_time=1
         },
@@ -93,16 +121,16 @@ local function stone_form(planet, cooling_time, cooled, source, source_count, re
     };
 
     local assembler_recipe = {
-        name=form_recipe_name,
+        name=form_name,
         type="recipe",
-        localised_name={"entity-name.form", {"item-name." .. result}, {"fluid-name."..source}},
-        localised_decription={"entity-description.cooling", {"item-name." .. result}, {"fluid-name."..source}},
-        main_product=form_item_name,
+        localised_name=cooling_name,
+        localised_decription=cooling_description,
+        main_product=form_name,
         ingredients={
             { type="item", name="stone", amount=4 }
         },
         results={
-            { type="item", name=form_item_name, amount=1 }
+            { type="item", name=form_name, amount=1 }
         },
         energy_required = 0.5,
         enabled = true,
@@ -110,10 +138,10 @@ local function stone_form(planet, cooling_time, cooled, source, source_count, re
     };
 
     local assembler_item = {
-        name=form_item_name,
+        name=form_name,
         type="item",
-        localised_name={"entity-name.form", {"item-name." .. result}, {"fluid-name."..source}},
-        localised_decription={"entity-description.cooling", {"item-name." .. result}, {"fluid-name."..source}},
+        localised_name=cooling_name,
+        localised_decription=cooling_description,
         icons=icons,
         enabled = true,
         stack_size = data.raw.item.stone.weight / 4 + 1,
@@ -125,16 +153,16 @@ local function stone_form(planet, cooling_time, cooled, source, source_count, re
     {
         name=cast_recipe_name,
         type="recipe",
-        localised_name={"entity-name.form", {"item-name." .. result}, {"fluid-name."..source}},
-        localised_decription={"entity-description.cooling", {"item-name." .. result}, {"fluid-name."..source}},
-        main_product=result,
+        localised_name=cooling_name,
+        localised_decription=cooling_description,
+        main_product=parameters.result.item,
         ingredients={
-            { type="fluid", name=source, amount=source_count, temperature=2000 }
+            { type="fluid", name=parameters.source.fluid, amount=parameters.source.amount, temperature=parameters.source.temperature }
         },
         results={
-            { type="item", name=result, amount=result_count }
+            { type="item", name=parameters.result.item, amount=parameters.result.amount }
         },
-        energy_required = cooling_time,
+        energy_required = parameters.cooling_time,
         enabled = true,
         category="nauvis-cast-category"
     }
@@ -156,6 +184,71 @@ data:extend{
     }
 };
 
-data:extend(stone_form("nauvis", 96, false, "molten-iron", 10, "iron-plate", 1));
-data:extend(stone_form("nauvis", 48, false, "molten-iron", 20, "iron-gear-wheel", 1));
-data:extend(stone_form("nauvis", 64, false, "molten-iron", 10, "iron-stick", 2));
+require("prototypes.pipecovers")
+require("prototypes.pipepictures")
+
+local sounds = require("__base__/prototypes/entity/sounds")
+
+data:extend{
+    {
+        name="nauvis-molten-channel",
+        type="pipe",
+        icon = "__base__/graphics/icons/stone.png",
+        flags = {"placeable-neutral", "player-creation"},
+        minable = {mining_time = 0.1, result = "nauvis-molten-channel"},
+        selection_box = {{-0.4, -0.4}, {0.4, 0.4}},
+        collision_box = {{-0.4, -0.4}, {0.4, 0.4}},
+        collision_mask = {layers = {item = true, meltable = true, object = true, water_tile = true, is_object = true, is_lower_object = true}},
+        fluid_box = {
+            volume=1,
+            pipe_covers=pipecoverspictures(),
+            pipe_connections =
+            {
+              { direction = defines.direction.north, position = {0, 0}, connection_category="molten-channel" },
+              { direction = defines.direction.east, position = {0, 0}, connection_category="molten-channel" },
+              { direction = defines.direction.south, position = {0, 0}, connection_category="molten-channel" },
+              { direction = defines.direction.west, position = {0, 0}, connection_category="molten-channel" }
+            },
+            hide_connection_info = true
+        },
+        pictures = pipepictures(),
+        impact_category = "stone",
+        working_sound = sounds.pipe,
+        --open_sound = sounds.stone_small_,
+        --close_sound = sounds.metal_small_close,
+    
+        horizontal_window_bounding_box = {{-0.25, -0.28125}, {0.25, 0.15625}},
+        vertical_window_bounding_box = {{-0.28125, -0.5}, {0.03125, 0.125}}
+    },
+    {
+        name="nauvis-molten-channel",
+        type="recipe",
+        main_product="nauvis-molten-channel",
+        ingredients={
+            { type="item", name="stone", amount=4 }
+        },
+        results={
+            { type="item", name="nauvis-molten-channel", amount=1 }
+        },
+        energy_required = 0.5,
+        enabled = true,
+        category="crafting"
+    },
+    {
+        name="nauvis-molten-channel",
+        type="item",
+        icons={
+            { icon="__base__/graphics/icons/stone.png" },
+        },
+        enabled = true,
+        stack_size = data.raw.item.stone.weight / 4 + 1,
+        weight = data.raw.item.stone.weight * 4,
+        place_result="nauvis-molten-channel"
+    }
+};
+
+data:extend(stone_form{ planet="nauvis", cooling_time=48, source={ fluid="molten-iron", amount=10, temperature=iron_temperature }, result={ item="iron-plate", amount=1 } });
+data:extend(stone_form{ planet="nauvis", cooling_time=96, source={ fluid="molten-iron", amount=20, temperature=iron_temperature }, result={ item="iron-gear-wheel", amount=1 } });
+data:extend(stone_form{ planet="nauvis", cooling_time=64, source={ fluid="molten-iron", amount=10, temperature=iron_temperature }, result={ item="iron-stick", amount=2 } });
+data:extend(stone_form{ planet="nauvis", cooling_time=32, source={ fluid="molten-copper", amount=10, temperature=copper_temperature }, result={ item="copper-plate", amount=1 } });
+data:extend(stone_form{ planet="nauvis", cooling_time=16, source={ fluid="molten-copper", amount=5, temperature=copper_temperature }, result={ item="copper-cable", amount=1 } });
---
+++ src/prototypes/pipepictures.lua
@@ -0,0 +1,239 @@
+function pipepictures()
+  return
+  {
+    straight_vertical_single =
+    {
+      filename = "__base__/graphics/entity/pipe/pipe-straight-vertical-single.png",
+      priority = "extra-high",
+      width = 160,
+      height = 160,
+      scale = 0.5
+    },
+    straight_vertical =
+    {
+      filename = "__base__/graphics/entity/pipe/pipe-straight-vertical.png",
+      priority = "extra-high",
+      width = 128,
+      height = 128,
+      scale = 0.5
+    },
+    straight_vertical_window =
+    {
+      filename = "__base__/graphics/entity/pipe/pipe-straight-vertical-window.png",
+      priority = "extra-high",
+      width = 128,
+      height = 128,
+      scale = 0.5
+    },
+    straight_horizontal_window =
+    {
+      filename = "__base__/graphics/entity/pipe/pipe-straight-horizontal-window.png",
+      priority = "extra-high",
+      width = 128,
+      height = 128,
+      scale = 0.5
+    },
+    straight_horizontal =
+    {
+      filename = "__base__/graphics/entity/pipe/pipe-straight-horizontal.png",
+      priority = "extra-high",
+      width = 128,
+      height = 128,
+      scale = 0.5
+    },
+    corner_up_right =
+    {
+      filename = "__base__/graphics/entity/pipe/pipe-corner-up-right.png",
+      priority = "extra-high",
+      width = 128,
+      height = 128,
+      scale = 0.5
+    },
+    corner_up_left =
+    {
+      filename = "__base__/graphics/entity/pipe/pipe-corner-up-left.png",
+      priority = "extra-high",
+      width = 128,
+      height = 128,
+      scale = 0.5
+    },
+    corner_down_right =
+    {
+      filename = "__base__/graphics/entity/pipe/pipe-corner-down-right.png",
+      priority = "extra-high",
+      width = 128,
+      height = 128,
+      scale = 0.5
+    },
+    corner_down_left =
+    {
+      filename = "__base__/graphics/entity/pipe/pipe-corner-down-left.png",
+      priority = "extra-high",
+      width = 128,
+      height = 128,
+      scale = 0.5
+    },
+    t_up =
+    {
+      filename = "__base__/graphics/entity/pipe/pipe-t-up.png",
+      priority = "extra-high",
+      width = 128,
+      height = 128,
+      scale = 0.5
+    },
+    t_down =
+    {
+      filename = "__base__/graphics/entity/pipe/pipe-t-down.png",
+      priority = "extra-high",
+      width = 128,
+      height = 128,
+      scale = 0.5
+    },
+    t_right =
+    {
+      filename = "__base__/graphics/entity/pipe/pipe-t-right.png",
+      priority = "extra-high",
+      width = 128,
+      height = 128,
+      scale = 0.5
+    },
+    t_left =
+    {
+      filename = "__base__/graphics/entity/pipe/pipe-t-left.png",
+      priority = "extra-high",
+      width = 128,
+      height = 128,
+      scale = 0.5
+    },
+    cross =
+    {
+      filename = "__base__/graphics/entity/pipe/pipe-cross.png",
+      priority = "extra-high",
+      width = 128,
+      height = 128,
+      scale = 0.5
+    },
+    ending_up =
+    {
+      filename = "__base__/graphics/entity/pipe/pipe-ending-up.png",
+      priority = "extra-high",
+      width = 128,
+      height = 128,
+      scale = 0.5
+    },
+    ending_down =
+    {
+      filename = "__base__/graphics/entity/pipe/pipe-ending-down.png",
+      priority = "extra-high",
+      width = 128,
+      height = 128,
+      scale = 0.5
+    },
+    ending_right =
+    {
+      filename = "__base__/graphics/entity/pipe/pipe-ending-right.png",
+      priority = "extra-high",
+      width = 128,
+      height = 128,
+      scale = 0.5
+    },
+    ending_left =
+    {
+      filename = "__base__/graphics/entity/pipe/pipe-ending-left.png",
+      priority = "extra-high",
+      width = 128,
+      height = 128,
+      scale = 0.5
+    },
+    straight_vertical_single_visualization = make_visualization(0),
+    straight_vertical_visualization = make_visualization(5),
+    straight_vertical_window_visualization = make_visualization(5),
+    straight_horizontal_window_visualization = make_visualization(10),
+    straight_horizontal_visualization = make_visualization(10),
+    corner_up_right_visualization = make_visualization(3),
+    corner_up_left_visualization = make_visualization(9),
+    corner_down_right_visualization = make_visualization(6),
+    corner_down_left_visualization = make_visualization(12),
+    t_up_visualization = make_visualization(11),
+    t_down_visualization = make_visualization(14),
+    t_right_visualization = make_visualization(7),
+    t_left_visualization = make_visualization(13),
+    cross_visualization = make_visualization(15),
+    ending_up_visualization = make_visualization(1),
+    ending_down_visualization = make_visualization(4),
+    ending_right_visualization = make_visualization(2),
+    ending_left_visualization = make_visualization(8),
+    straight_vertical_single_disabled_visualization = make_disabled_visualization(0),
+    straight_vertical_disabled_visualization = make_disabled_visualization(5),
+    straight_vertical_window_disabled_visualization = make_disabled_visualization(5),
+    straight_horizontal_window_disabled_visualization = make_disabled_visualization(10),
+    straight_horizontal_disabled_visualization = make_disabled_visualization(10),
+    corner_up_right_disabled_visualization = make_disabled_visualization(3),
+    corner_up_left_disabled_visualization = make_disabled_visualization(9),
+    corner_down_right_disabled_visualization = make_disabled_visualization(6),
+    corner_down_left_disabled_visualization = make_disabled_visualization(12),
+    t_up_disabled_visualization = make_disabled_visualization(11),
+    t_down_disabled_visualization = make_disabled_visualization(14),
+    t_right_disabled_visualization = make_disabled_visualization(7),
+    t_left_disabled_visualization = make_disabled_visualization(13),
+    cross_disabled_visualization = make_disabled_visualization(15),
+    ending_up_disabled_visualization = make_disabled_visualization(1),
+    ending_down_disabled_visualization = make_disabled_visualization(4),
+    ending_right_disabled_visualization = make_disabled_visualization(2),
+    ending_left_disabled_visualization = make_disabled_visualization(8),
+    horizontal_window_background =
+    {
+      filename = "__base__/graphics/entity/pipe/pipe-horizontal-window-background.png",
+      priority = "extra-high",
+      width = 128,
+      height = 128,
+      scale = 0.5
+    },
+    vertical_window_background =
+    {
+      filename = "__base__/graphics/entity/pipe/pipe-vertical-window-background.png",
+      priority = "extra-high",
+      width = 128,
+      height = 128,
+      scale = 0.5
+    },
+    fluid_background =
+    {
+      filename = "__base__/graphics/entity/pipe/fluid-background.png",
+      priority = "extra-high",
+      width = 64,
+      height = 40,
+      scale = 0.5
+    },
+    low_temperature_flow =
+    {
+      filename = "__base__/graphics/entity/pipe/fluid-flow-low-temperature.png",
+      priority = "extra-high",
+      width = 160,
+      height = 18
+    },
+    middle_temperature_flow =
+    {
+      filename = "__base__/graphics/entity/pipe/fluid-flow-medium-temperature.png",
+      priority = "extra-high",
+      width = 160,
+      height = 18
+    },
+    high_temperature_flow =
+    {
+      filename = "__base__/graphics/entity/pipe/fluid-flow-high-temperature.png",
+      priority = "extra-high",
+      width = 160,
+      height = 18
+    },
+    gas_flow =
+    {
+      filename = "__base__/graphics/entity/pipe/steam.png",
+      priority = "extra-high",
+      line_length = 10,
+      width = 48,
+      height = 30,
+      frame_count = 60
+    }
+  }
+end
